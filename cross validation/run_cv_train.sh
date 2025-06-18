6cfdd8174e9f49d0d5e75c4fdaab0369a504a9fc
shuhei-hara <57740458+shuhei-hara@users.noreply.github.com>
2025-06-18 14:27:27 +0900

    Update run_cv_train.sh

--- cross validation/run_cv_train.sh
+++ cross validation/run_cv_train.sh
@@ -5,7 +5,7 @@
 #SBATCH -n 1                             
 #SBATCH --cpus-per-task=4                
 #SBATCH --mem-per-cpu=32G                
-#SBATCH --time=4:00:00                   
+#SBATCH --time=estimated_time                   
 #SBATCH --mail-type=ALL                  
 #SBATCH -o log/output-%A-%a.txt          
 
